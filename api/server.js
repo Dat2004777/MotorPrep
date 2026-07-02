f599624d00cc0331769a609c90d50c07e6babb1d
Nguyễn DatJ <dat2004777@gmail.com>
2026-07-02 21:10:53 +0700

    fix: database for vercel

--- api/server.js
+++ api/server.js
@@ -1,8 +1,14 @@
 // api/server.js
 import jsonServer from "json-server";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = jsonServer.create();
-const router = jsonServer.router("database.json");
+
+const router = jsonServer.router(path.join(__dirname, "database.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
